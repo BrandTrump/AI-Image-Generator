4adef61b49ff8bf59d1a02696aa387cc807944f4
Brandon Trump <brandontrump101@gmail.com>
2023-04-18 15:51:00 +1200

    feat: fetchs azure function

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,6 +1,9 @@
 export async function GET(request: Request) {
   // connect to Microsoft Azure function endpoint
-  const respose = await fetch("...", { cache: "no-store" });
+  const respose = await fetch(
+    "http://localhost:7071/api/getChatGPTSuggestion",
+    { cache: "no-store" }
+  );
 
   const textData = await respose.text();
   return new Response(JSON.stringify(textData.trim()), { status: 200 });
