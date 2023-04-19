54bd5cfccc66f44c5243a3bd1d3be615321bd978
Brandon Trump <brandontrump101@gmail.com>
2023-04-19 15:35:31 +1200

    feat: created post route that sends a prompt

---
+++ app/api/generateImage/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  const prompt = res.prompt;
+
+  // connect to Microsoft Azure function endpoint
+  const respose = await fetch("/api/generateImage", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ prompt }),
+  });
+
+  const textData = await respose.text();
+  return NextResponse.json(textData);
+}
