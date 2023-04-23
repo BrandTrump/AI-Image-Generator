9b6dbd27f5ab62db98ad20c7321ceb996a316fc9
Brandon Trump <brandontrump101@gmail.com>
2023-04-23 22:50:10 +1200

    feat: created image component

---
+++ components/Images.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Image from "next/image";
+import useSWR from "swr";
+import fetchImages from "../lib/fetchImages";
+
+function Images() {
+  return <div>Images</div>;
+}
+
+export default Images;
